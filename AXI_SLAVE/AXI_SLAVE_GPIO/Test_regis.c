2fdbc2a1028c1b3cc9b88834de0e89354d548655
angerpro1411 <166725219+angerpro1411@users.noreply.github.com>
2024-06-12 16:19:23 +0200

    Update Test_regis.c

--- AXI_SLAVE/AXI_SLAVE_GPIO/Test_regis.c
+++ AXI_SLAVE/AXI_SLAVE_GPIO/Test_regis.c
@@ -1,9 +1,9 @@
 /******************************************************************************
-*Test simple in the enviroment Vitis with base address of Slave Register. 
+*Test simple in the enviroment Vitis with base address of Slave Register.
 *We can see inside library "xparameters.h"
 * Peripheral Definitions for peripheral AXI_LITE_SLAVE_VHD_O_0
-#define XPAR_AXI_LITE_SLAVE_VHD_O_0_BASEADDR 0x40000000
-#define XPAR_AXI_LITE_SLAVE_VHD_O_0_HIGHADDR 0x40000FFF
+#define XPAR_AXI_LITE_SLAVE_LEDS_SWITCHES_0_BASEADDR 0x40000000
+#define XPAR_AXI_LITE_SLAVE_LEDS_SWITCHES_0_HIGHADDR 0x40000FFF
 * 0x40000000 is the address of Reg0 and +4 next for next reg(4 regs in total design).
 * The example that we and 8 lsb bit of reg2 and 8 lsb bit of reg3, and get the output to Leds of Zedboard.
 ******************************************************************************/
@@ -37,6 +37,7 @@
 #include <stdio.h>
 #include "xil_printf.h"
 #include "xparameters.h"
+#include "sleep.h"
 
 volatile unsigned int *slave_reg00;
 volatile unsigned int *slave_reg01;
@@ -49,36 +50,46 @@ volatile unsigned int *SW;
 int main()
 {
 	//Start program.
-	xil_printf("/****************Program starts*****************/\r\n");
+	xil_printf("\r\n/-------------------Program starts----------------/");
 	unsigned int LEDs = 0;
 	unsigned int REG00,REG03 = 0;
 	/*
 	 * Point the register correspond to each address.
 	 */
-	slave_reg00 = (volatile unsigned int *)XPAR_AXI_LITE_SLAVE_VHD_O_0_BASEADDR;
-	slave_reg01 = (volatile unsigned int *)(XPAR_AXI_LITE_SLAVE_VHD_O_0_BASEADDR + 4);
-	slave_reg02 = (volatile unsigned int *)(XPAR_AXI_LITE_SLAVE_VHD_O_0_BASEADDR + 8);
-	slave_reg03 = (volatile unsigned int *)(XPAR_AXI_LITE_SLAVE_VHD_O_0_BASEADDR + 12);
+	slave_reg00 = (volatile unsigned int *)XPAR_AXI_LITE_SLAVE_LEDS_SWITCHES_0_BASEADDR;
+	slave_reg01 = (volatile unsigned int *)(XPAR_AXI_LITE_SLAVE_LEDS_SWITCHES_0_BASEADDR + 4);
+	slave_reg02 = (volatile unsigned int *)(XPAR_AXI_LITE_SLAVE_LEDS_SWITCHES_0_BASEADDR + 8);
+	slave_reg03 = (volatile unsigned int *)(XPAR_AXI_LITE_SLAVE_LEDS_SWITCHES_0_BASEADDR + 12);
 
 	//SW shares the same address "00" in hardware specification. Read VHDL code to more understanding.
-	SW = (volatile unsigned int *)XPAR_AXI_LITE_SLAVE_VHD_O_0_BASEADDR;
+	SW = (volatile unsigned int *)XPAR_AXI_LITE_SLAVE_LEDS_SWITCHES_0_BASEADDR;
 
 	/*
 	 * 7LEDs display. In the hardware specification, we define that 7-Leds = REG00 and REG03.
 	 * So change the value both of them will change the LEDs display in Zedboard.
 	 */
-	REG00 = 0xFF;
-	REG03 = 0x22;
+	while(1){
+		xil_printf("\r\nWrite 8-bits value of Register00 0x");
+		scanf("%x",&REG00);
+		xil_printf("\r\nYou write 8-bits value of Register00 0x%x",REG00);
 
-	//Write to real register in Hardware.
-	*slave_reg03 = REG03;
-	*slave_reg00 = REG00;
-	LEDs = REG00 & REG03;
-	xil_printf("LEDs will display 0x%x\r\n", LEDs);
+		xil_printf("\r\nWrite 8-bits value of Register03 0x");
+		scanf("%x",&REG03);
+		xil_printf("\r\nYou write 8-bits value of Register03 0x%x",REG03);
 
-	//Read the status of Switches.
-	xil_printf("The value of 7-switches is 0x%x\r\n",*SW);
+		//Write to real register in Hardware.
+		*slave_reg03 = REG03;
+		*slave_reg00 = REG00;
+		LEDs = REG00 & REG03;
+		xil_printf("\r\nLEDs will display 0x%x", LEDs);
 
+		//Read the status of Switches.
+		xil_printf("\r\nThe value of 7-switches is 0x%x",*SW);
+
+		xil_printf("\r\n-----------------------------------");
+
+		//delay for next ask
+		sleep(1);
+	}
     return 0;
 }
-
